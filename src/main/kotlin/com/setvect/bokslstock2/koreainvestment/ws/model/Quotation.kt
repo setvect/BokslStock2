46a2823dc90ed36ebc40ced6eced37c7e9c53859
setvect <setvect@naver.com>
2022-09-04 12:35:28 +0900

    웹소켓 데이터 파싱

--- src/main/kotlin/com/setvect/bokslstock2/koreainvestment/ws/model/Quotation.kt
+++ src/main/kotlin/com/setvect/bokslstock2/koreainvestment/ws/model/Quotation.kt
@@ -1,10 +1,18 @@
 package com.setvect.bokslstock2.koreainvestment.ws.model
 
+import com.setvect.bokslstock2.util.DateUtil
+import org.apache.commons.lang3.StringUtils
+import java.time.LocalTime
+import java.util.stream.IntStream
+import kotlin.streams.toList
+
 /**
  * 실시간 호가
  */
 data class Quotation(
     val code: String, // 종목코드
+    val bsopHour: LocalTime, // 영업 시간
+    val hourClsCode: String, // 시간 구분 코드
     val askPrice: Map<Int, Quote>, // 매도호가 10단계, Key: 10 ~ 1, 10 높은가격 1 낮은가격
     val bidPrice: Map<Int, Quote>, // 매수호가 10단계, Key: 1 ~ 10, 1 높은가격 10 낮은가격
     val totalAskpRsqn: Long, // 총 매도호가 잔량
@@ -32,27 +40,38 @@ data class Quotation(
          * [rawText] 233740^152809^A^8960^8965^8970^0^0^0^0^0^0^0^8955^8950^8945^0^0^0^0^0^0^0^29^636^684^0^0^0^0^0^0^0^1070^14889^10942^0^0^0^0^0^0^0^0^0^0^0^8955^229477^229477^180^2^2.05^12263346^0^0^0^0^0
          */
         fun parsing(rawText: String): Quotation {
+            val dataArray = StringUtils.split(rawText, "^")
+
+            val askPrice = IntStream.range(0, 10)
+                .mapToObj { p: Int -> p }
+                .toList().associate { it + 1 to Quote(dataArray[it + 3].toLong(), dataArray[it + 23].toLong()) }
+
+            val bidPrice = IntStream.range(0, 10)
+                .mapToObj { p: Int -> p }
+                .toList().associate { it + 1 to Quote(dataArray[it + 13].toLong(), dataArray[it + 33].toLong()) }
 
             return Quotation(
-                code = "100",
-                askPrice = mapOf(),
-                bidPrice = mapOf(),
-                totalAskpRsqn = 100,
-                totalBidpRsqn = 100,
-                ovtmTotalAskpRsqn = 100,
-                ovtmTotalBidpRsqn = 100,
-                antcCnpr = 100,
-                antcCnqn = 100,
-                antcVol = 100,
-                antcCntgVrss = 100.0,
-                antcCntgVrssSign = "1",
-                antcCntgPrdyCtrt = 100.0,
-                acmlVol = 100,
-                totalAskpRsqnIcdc = 100,
-                totalBidpRsqnIcdc = 100,
-                ovtmTotalAskpIcdc = 100,
-                ovtmTotalBidpIcdc = 100,
-                stckDealClsCode = "AA",
+                code = dataArray[0],
+                bsopHour = DateUtil.getLocalTime(dataArray[1], DateUtil.HHmmss),
+                hourClsCode = dataArray[2],
+                askPrice = askPrice,
+                bidPrice = bidPrice,
+                totalAskpRsqn = dataArray[43].toLong(),
+                totalBidpRsqn = dataArray[44].toLong(),
+                ovtmTotalAskpRsqn = dataArray[45].toLong(),
+                ovtmTotalBidpRsqn = dataArray[46].toLong(),
+                antcCnpr = dataArray[47].toLong(),
+                antcCnqn = dataArray[48].toLong(),
+                antcVol = dataArray[49].toLong(),
+                antcCntgVrss = dataArray[50].toDouble(),
+                antcCntgVrssSign = dataArray[51],
+                antcCntgPrdyCtrt = dataArray[52].toDouble(),
+                acmlVol = dataArray[53].toLong(),
+                totalAskpRsqnIcdc = dataArray[54].toLong(),
+                totalBidpRsqnIcdc = dataArray[55].toLong(),
+                ovtmTotalAskpIcdc = dataArray[56].toLong(),
+                ovtmTotalBidpIcdc = dataArray[57].toLong(),
+                stckDealClsCode = dataArray[58],
             )
         }
     }
@@ -61,6 +80,6 @@ data class Quotation(
     /**
      * [count] 수량, [price] 가격
      */
-    data class Quote(val count: Long, val price: Double)
+    data class Quote(val price: Long, val count: Long)
 }
 
--- src/test/kotlin/com/setvect/bokslstock2/koreainvestment/ws/model/QuotationTest.kt
+++ src/test/kotlin/com/setvect/bokslstock2/koreainvestment/ws/model/QuotationTest.kt
@@ -10,5 +10,20 @@ internal class QuotationTest {
             "233740^152809^A^8960^8965^8970^0^0^0^0^0^0^0^8955^8950^8945^0^0^0^0^0^0^0^29^636^684^0^0^0^0^0^0^0^1070^14889^10942^0^0^0^0^0^0^0^0^0^0^0^8955^229477^229477^180^2^2.05^12263346^0^0^0^0^0"
         val quotation = Quotation.parsing(rawText)
         Assertions.assertThat(quotation.code).isEqualTo("233740")
+        Assertions.assertThat(quotation.askPrice[1]?.price).isEqualTo(8960)
+        Assertions.assertThat(quotation.askPrice[1]?.count).isEqualTo(29)
+        Assertions.assertThat(quotation.askPrice[3]?.price).isEqualTo(8970)
+        Assertions.assertThat(quotation.askPrice[3]?.count).isEqualTo(684)
+        Assertions.assertThat(quotation.askPrice[10]?.price).isEqualTo(0)
+        Assertions.assertThat(quotation.askPrice[10]?.count).isEqualTo(0)
+
+        Assertions.assertThat(quotation.bidPrice[1]?.price).isEqualTo(8955)
+        Assertions.assertThat(quotation.bidPrice[1]?.count).isEqualTo(1070)
+        Assertions.assertThat(quotation.bidPrice[3]?.price).isEqualTo(8945)
+        Assertions.assertThat(quotation.bidPrice[3]?.count).isEqualTo(10942)
+        Assertions.assertThat(quotation.bidPrice[10]?.price).isEqualTo(0)
+        Assertions.assertThat(quotation.bidPrice[10]?.count).isEqualTo(0)
+
+        Assertions.assertThat(quotation.antcCnpr).isEqualTo(8955)
     }
 }
