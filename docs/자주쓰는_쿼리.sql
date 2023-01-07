168c9fa3be06cc61f9be0a5a03dbbcaf87e42a23
setvect <setvect@naver.com>
2023-01-07 19:41:21 +0900

    분석용 쿼리 추가

--- docs/자주쓰는_쿼리.sql
+++ docs/자주쓰는_쿼리.sql
@@ -4,3 +4,43 @@ from CA_STOCK CA
          join CB_CANDLE CC on CA.STOCK_SEQ = CC.STOCK_SEQ
 group by CA.STOCK_SEQ
 ;
+
+-- 개장 최소 5분봉 시초가와 종가 비교
+
+select CA.CODE,
+       CA.NAME,
+       CB.PERIOD_TYPE,
+       CB.CANDLE_DATE_TIME,
+       FORMATDATETIME(CB.CANDLE_DATE_TIME, 'HH:mm') hh_mm,
+       CB.OPEN_PRICE,
+       CB.CLOSE_PRICE,
+       CB.CLOSE_PRICE - CB.OPEN_PRICE               DIFF
+from CA_STOCK CA
+         join CB_CANDLE CB on CA.STOCK_SEQ = CB.STOCK_SEQ
+where CA.CODE = '091170'
+  and PERIOD_TYPE = 'PERIOD_MINUTE_5'
+  and FORMATDATETIME(CB.CANDLE_DATE_TIME, 'HH:mm') = '09:05';
+
+select CODE,
+       NAME,
+       count(*),
+       sum(DIFF),
+       avg(DIFF),
+       sum(CASEWHEN(DIFF > 0, 1, 0)) `증가`,
+       sum(CASEWHEN(DIFF < 0, 1, 0)) `감소`,
+       sum(CASEWHEN(DIFF = 0, 1, 0)) `같음`
+from (select CA.CODE,
+             CA.NAME,
+             CB.PERIOD_TYPE,
+             CB.CANDLE_DATE_TIME,
+             FORMATDATETIME(CB.CANDLE_DATE_TIME, 'HH:mm') hh_mm,
+             CB.OPEN_PRICE,
+             CB.CLOSE_PRICE,
+             CB.CLOSE_PRICE - CB.OPEN_PRICE               DIFF
+      from CA_STOCK CA
+               join CB_CANDLE CB on CA.STOCK_SEQ = CB.STOCK_SEQ
+      where 1 = 1
+--         and CA.CODE = '091170'
+        and PERIOD_TYPE = 'PERIOD_MINUTE_5'
+        and FORMATDATETIME(CB.CANDLE_DATE_TIME, 'HH:mm') = '09:05') AA
+group by CODE;
